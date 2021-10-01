d2e92ec8b673fb4116d62bb4e00557498a651419
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2021-10-01 21:49:00 +0530

    Add test case

---
+++ java-app/src/test/java/com/gl/PrimeNumberTest.java
@@ -0,0 +1,20 @@
+package com.gl;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void isPrimeTrue(){
+        assertTrue(PrimeNumber.isPrime(5));
+    }
+    @Test
+    public void isPrimeFalse(){
+        assertFalse(PrimeNumber.isPrime(15));
+    }
+    @Test
+    public void first15PrimeNumbers(){
+        assertEquals(15,PrimeNumber.primeNumbers(50).size());
+    }
+}
