54a6f17d73fa6e5be8dda7256b7fa03214044b0e
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-07-21 10:57:35 +0530

    find All Characters And Count Of The Character in given string input

--- java-app/src/main/java/com/gl/Java2023Session25/Sample.java
+++ java-app/src/main/java/com/gl/Java2023Session25/Sample.java
@@ -4,8 +4,13 @@ public class Sample {
     public static void main(String[] args) {
         String input = "This is a test message for testing";
         System.out.println("Input string = " + input);
+        System.out.println("----------------------------------------------------------");
 
         findAllDuplicateCharactersAndCountOfTheDuplicateCharacter(input);
+        System.out.println("----------------------------------------------------------");
+
+        findAllCharactersAndCountOfTheCharacter(input);
+        System.out.println("----------------------------------------------------------");
     }
 
     static void findAllDuplicateCharactersAndCountOfTheDuplicateCharacter(String input) {
@@ -20,6 +25,22 @@ public class Sample {
         for (int i = 0; i < allCharactersArray.length; i++) {
             if (allCharactersArray[i] > 1) {
                 System.out.println("Duplicate character is : [" + (char) i + "], its count is : [" + allCharactersArray[i] + "] times");
+            }
+        }
+    }
+
+    static void findAllCharactersAndCountOfTheCharacter(String input) {
+        int[] allCharactersArray = new int[256];
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            int asciiValueOfCharAsAnIndexPositionOfArray = (int) ch;
+            allCharactersArray[asciiValueOfCharAsAnIndexPositionOfArray] = allCharactersArray[asciiValueOfCharAsAnIndexPositionOfArray] + 1;
+        }
+
+        for (int i = 0; i < allCharactersArray.length; i++) {
+            if (allCharactersArray[i] > 0) {
+                System.out.println("Character is : [" + (char) i + "], its count is : [" + allCharactersArray[i] + "] times");
             }
         }
     }
