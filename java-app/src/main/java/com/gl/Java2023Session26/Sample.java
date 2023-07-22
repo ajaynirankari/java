efd80cc9d3793e75d5cabb181ff79885fb76d953
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-07-22 10:24:37 +0530

    insertion sort for give input of int array

---
+++ java-app/src/main/java/com/gl/Java2023Session26/Sample.java
@@ -0,0 +1,29 @@
+package com.gl.Java2023Session26;
+
+import java.util.Arrays;
+
+public class Sample {
+    public static void main(String[] args) {
+
+        int[] input = {6, 2, 9, 4, 1, 3, 5};
+        System.out.println("input = " + Arrays.toString(input));
+
+        insertionSort(input);
+        System.out.println("input = " + Arrays.toString(input));
+
+        // Time complexity    ------   depend upon the number of elements
+        // Space complexity   ------   depend upon the number of elements
+    }
+
+    static void insertionSort(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && key < a[j]) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = key;
+        }
+    }
+}
