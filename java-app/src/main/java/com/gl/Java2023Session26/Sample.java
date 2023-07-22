5ad7a4653987ee896ec5252dc925c8cd9e0ed067
Ajay.Nirankari <ajaykumarnirankarigl@gmail.com>
2023-07-22 10:29:21 +0530

    insertion sort in descending order

--- java-app/src/main/java/com/gl/Java2023Session26/Sample.java
+++ java-app/src/main/java/com/gl/Java2023Session26/Sample.java
@@ -8,18 +8,33 @@ public class Sample {
         int[] input = {6, 2, 9, 4, 1, 3, 5};
         System.out.println("input = " + Arrays.toString(input));
 
-        insertionSort(input);
+        insertionSortInAscendingOrder(input);
+        System.out.println("input = " + Arrays.toString(input));
+
+        insertionSortInDescendingOrder(input);
         System.out.println("input = " + Arrays.toString(input));
 
         // Time complexity    ------   depend upon the number of elements
         // Space complexity   ------   depend upon the number of elements
     }
 
-    static void insertionSort(int[] a) {
+    static void insertionSortInAscendingOrder(int[] a) {
         for (int i = 1; i < a.length; i++) {
             int key = a[i];
             int j = i - 1;
             while (j >= 0 && key < a[j]) {
+                a[j + 1] = a[j];
+                j = j - 1;
+            }
+            a[j + 1] = key;
+        }
+    }
+
+    static void insertionSortInDescendingOrder(int[] a) {
+        for (int i = 1; i < a.length; i++) {
+            int key = a[i];
+            int j = i - 1;
+            while (j >= 0 && key > a[j]) {
                 a[j + 1] = a[j];
                 j = j - 1;
             }
